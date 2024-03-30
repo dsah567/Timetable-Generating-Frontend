ed015e9d1dc94fd2d1e27429d6e5367431535e1d
Dipu sah <sahd7929@gmail.com>
2024-03-30 17:00:40 +0530

    Signup and login componenet modified

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Form} from  "react-router-dom"
+import {Form,Navigate,redirect,useActionData,useSearchParams} from  "react-router-dom"
 import axios from "axios"
 
 export async function  loginAction({request}){
@@ -9,27 +9,35 @@ export async function  loginAction({request}){
             email:formData.get("email"),
             password:formData.get("password")
         })
-        .then((res)=>console.log(res.data))
+        .then((res)=>{console.log(res.data)
+             return window.location.href="/"}
+            )
         .catch(function (error) {
-            console.log(error);
+            console.log("axios",error);
+            return window.location.href="/login?invalidDetails=Invalid_User_Creditanls"
         });
-        console.log(result);
-        return window.location.href="/"
+        
     }
     catch(err){
         console.log(err);
+        return window.location.href="/login?invalidDetails=Something_Went_Wrong"
     }
      
 }
 
 export default function Login() {
+    let [searchParams, setSearchParams] = useSearchParams();
+    let invalidDetails= searchParams.get("invalidDetails")
+    console.log(invalidDetails);
     return (
+        
         <div className=" bg-gradient-to-br from-purple-500 to-pink-500">
 
         <div className="flex justify-center items-center h-screen ">
 
         <Form className="bg-gray-200 shadow-md rounded px-8 pt-6 pb-8 mb-4" method="post" >
         <div className="mb-4">
+            {invalidDetails && <label className="block text-red-700 text-sm font-bold mb-2">{invalidDetails}</label> }
             <label className="block text-gray-700 text-sm font-bold mb-2">Enter Your Email</label>
             <input type="email" name="email" placeholder='Enter email or username'
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"/>
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import {Form, redirect} from  "react-router-dom"
+import {Form, redirect,useSearchParams} from  "react-router-dom"
 import axios from "axios"
 
 export async function  signupAction({request}){
     let formData = await request.formData();
+    let Redirect = null
     try{
         let result =await  axios.post("/api/v1/users/register",{
             name:formData.get("name"),
@@ -11,20 +12,33 @@ export async function  signupAction({request}){
             password:formData.get("password"),
             username:formData.get("username"),
         })
-        .then((res)=>console.log(res.data))
-        .catch(function (error) {
-            console.log(error);
+        .then((res)=>{
+            console.log(res.data)
+           Redirect="/login"
+            })
+        .catch((error)=> {
+            console.log("Axios Error Signup",error);
+            if(error.response.status==400)
+                Redirect = "/signup?details=Input_Every_Fields"
+            else if(error.response.status==409)
+                Redirect = "/signup?details=User_with_email_or_username_exist"
+            else
+                Redirect = "/signup"
         });
         console.log(result);
-        return redirect("/")
+        return redirect(Redirect)
     }
     catch(err){
         console.log(err);
+        return redirect("/")
     }
      
 }
 
 function Signup() {
+    let [searchParams, setSearchParams] = useSearchParams();
+    let details= searchParams.get("details")
+    console.log(details);
     return (
         <div className=" bg-gradient-to-br from-purple-500 to-pink-500">
 
@@ -32,6 +46,7 @@ function Signup() {
 
         <Form className="bg-gray-200 shadow-md rounded px-8 pt-6 pb-8 mb-4" method="post" >
         <div className="mb-4">
+        {details && <label className="block text-red-700 text-sm font-bold mb-2">{details}</label> }
             <label className="block text-gray-700 text-sm font-bold mb-2">Enter Department Name</label>
             <input type="text" name="name" placeholder='Enter Department Name'
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"/>
